36b023dc6a5d7f1243d79bc1fc26c3742f99bcbd
Jake Beamish <jakebeamish@gmail.com>
2023-12-18 01:53:53 +0000

    Add light/dark mode

--- sketch.js
+++ sketch.js
@@ -2,9 +2,21 @@ const title = 'Untitled'
 
 let seed;
 let center;
+let darkMode;
+
+const colours = {
+    light: 230,
+    dark: 12
+}
+
+
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, SVG)
+    canvas = createCanvas(windowWidth, windowHeight, SVG)
+    canvas.position(0, 0)
+	canvas.style('position', 'fixed')
+	canvas.style('z-index', -1)
+
     seed = floor(random(1000000))
     randomSeed(seed)
     noiseSeed(seed)
@@ -14,16 +26,35 @@ function setup() {
 
 function draw() {
     clear()
+
+    if (darkMode) {
+        background(colours.dark)
+        stroke(colours.light)
+    } else {
+        background(colours.light)
+        stroke(colours.light)
+    }
+
     document.title = title;
     randomSeed(seed)
     noiseSeed(seed)
 }
 
 function keyPressed() {
+
+    // Press arrow keys to change the seed
     if (keyCode === 39) seed++; redraw()
     if (keyCode === 37) seed--; redraw()
+
+    // Press 'S' key to save SVG file
     if (keyCode === 83) {
         save(`${filename()}.svg`)
+    }
+
+    // Press 'D' key to toggle dark mode
+    if (keyCode === 68) {
+        darkMode = !darkMode;
+        redraw()
     }
 }
 
